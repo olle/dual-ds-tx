b28f887eb287f6e55e8d2912e74cee7a6b172188
Olle Törnström <olle@studiomediatech.com>
2025-02-15 15:48:37 +0100

    Configure tx mgrs

--- Makefile
+++ Makefile
@@ -7,3 +7,11 @@ run:
 .PHONY: test t verify v
 test t verify v:
 	${MVN} clean verify
+
+.PHONY: down
+down:
+	docker compose down --volumes --remove-orphans
+
+.PHONY: up
+up:
+	docker compose up -d
--- src/main/java/com/example/dual_ds_tx/DualDsTxApplication.java
+++ src/main/java/com/example/dual_ds_tx/DualDsTxApplication.java
@@ -2,6 +2,7 @@ package com.example.dual_ds_tx;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,32 +10,52 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 public class DualDsTxApplication {
 
+	static final String ALPHA_DATA_SOURCE = "alphaDataSource";
+	static final String OMEGA_DATA_SOURCE = "omegaDataSource";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DualDsTxApplication.class, args);
 	}
-	
+
 	@Configuration
 	static class DataSourceConfig {
-		
+
 		@Primary
-		@Bean("alpha")
+		@Bean(ALPHA_DATA_SOURCE)
 		@ConfigurationProperties(prefix = "spring.datasource.alpha")
 		public DataSource alpha() {
 			return DataSourceBuilder.create().build();
 		}
-		
+
 		@Primary
-		@Bean("omega")
+		@Bean(OMEGA_DATA_SOURCE)
 		@ConfigurationProperties(prefix = "spring.datasource.omega")
 		public DataSource omega() {
 			return DataSourceBuilder.create().build();
 		}
+	}
 
-		
+	@Configuration
+	static class TransactionManagerConfig {
+
+		@Primary
+		@Bean(name = "alphaTransactionManager")
+		public PlatformTransactionManager alphaTransactionManager(
+				@Qualifier(ALPHA_DATA_SOURCE) DataSource alphaDataSource) {
+			return new DataSourceTransactionManager(alphaDataSource);
+		}
+
+		@Bean(name = "omegaTransactionManager")
+		public PlatformTransactionManager omegaTransactionManager(
+				@Qualifier(OMEGA_DATA_SOURCE) DataSource omegaDataSource) {
+			return new DataSourceTransactionManager(omegaDataSource);
+		}
 	}
 
 }
